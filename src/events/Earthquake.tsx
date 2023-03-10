c250b32e8c72fa3bcae307fb9fc641557dd0d145
njmali <42544158+Netra2104@users.noreply.github.com>
2023-03-10 09:39:10 -0500

    trivial change

--- src/events/Earthquake.tsx
+++ src/events/Earthquake.tsx
@@ -16,7 +16,7 @@ export function Earthquake({ finish }: EarthquakeProps) {
       <p>A small earthquake has struck Jollywood!</p>
       <p>
         The city is mostly okay, except for one old apartment building that
-        collapsed, killing 55 people.
+        collapsed, killing 50 people.
       </p>
     </div>
   );

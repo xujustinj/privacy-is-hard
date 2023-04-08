4fe328bea479dc41dccc6f2fcd0919bffbb786e2
Justin Xu <xu.justin.j@gmail.com>
2023-04-08 03:25:53 -0400

    Add our names to the starting card

--- src/events/GoodEnd.tsx
+++ src/events/GoodEnd.tsx
@@ -1,9 +1,5 @@
-import { useEffect } from "react";
-import { BaseEventProps } from "./BaseEvent";
-
-export function GoodEnd({ finish }: BaseEventProps) {
-  useEffect(finish, [finish]);
-
+export function GoodEnd() {
+  // does not call finish() because the game is over
   return (
     <div>
       <p>Good job! You've mastered the art of private celebrity life.</p>
--- src/events/Start.tsx
+++ src/events/Start.tsx
@@ -11,6 +11,15 @@ const HollywoodText = styled.span`
   font-family: "SF Hollywood Hills";
 `;
 
+const Subtitle = styled.p`
+  text-align: center;
+
+  > a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
 export function Start({ finish }: BaseEventProps) {
   useEffect(finish, [finish]);
 
@@ -21,6 +30,28 @@ export function Start({ finish }: BaseEventProps) {
         <br />
         <HollywoodText>JOLLYWOOD</HollywoodText>
       </Title>
+      <Subtitle>
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/estelle-zk-w/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Estelle
+        </a>
+        ,{" "}
+        <a href="https://justinxu.me/" target="_blank" rel="noreferrer">
+          Justin
+        </a>
+        , and{" "}
+        <a
+          href="https://www.linkedin.com/in/netramali/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netra
+        </a>
+      </Subtitle>
       <p>
         Hi there! Welcome to the life of a celebrity. Your biggest fans are
         always watching - will you be able to keep up with privacy and still be
